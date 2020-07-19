77ea5a5e39942e4fae7536de3e5ba7485729c8d8
MobCode100 <dastur07@gmail.com>
2020-07-19 12:16:50 +0800

    LASTO

--- 2r2Mart/register_employee.php
+++ 2r2Mart/register_employee.php
@@ -80,6 +80,7 @@ if ($job !== '[null,""]') {
 
 <head>
   <title>2r2 Mart</title>
+  <link rel="icon" href="img/logo2.png"></title>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="stylesheet" href="css/bootstrap.min.css" />
